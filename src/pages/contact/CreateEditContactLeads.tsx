3a8fa85aab4814919226433c1dc2680cc2196f0c
Dystian En <distian.enyus@gmail.com>
2024-12-13 09:40:15 +0700

    fix: validation contact leads

--- src/pages/contact/CreateEditContactLeads.tsx
+++ src/pages/contact/CreateEditContactLeads.tsx
@@ -1,11 +1,6 @@
 import { LoadIndicator } from "devextreme-react";
-import Form, {
-  AsyncRule,
-  ButtonItem,
-  ButtonOptions,
-  GroupItem,
-  SimpleItem
-} from "devextreme-react/form";
+import Form, { ButtonItem, ButtonOptions, GroupItem, SimpleItem } from "devextreme-react/form";
+import { AsyncRule } from "devextreme-react/validator";
 import queryString from "query-string";
 import { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -14,6 +9,7 @@ import {
   createContactLeads,
   createLeads,
   updateLeads,
+  validateIdNumber,
   validatePhone
 } from "src/api/contact";
 import { InitLeadsValue, type TReqCreateLeads } from "src/interfaces/contactDto";
@@ -61,14 +57,20 @@ const CreateEditContactLeads = () => {
     setLoadingCreateContact(false);
   };
 
-  const handleSave = async () => {
+  const handleSave = () => {
     try {
-      setLoadingSave(true);
-      if (isCreate) {
-        await createLeads(leads).then(handleSuccess).catch(handleError);
-      } else {
-        await updateLeads(idData, leads).then(handleSuccess).catch(handleError);
-      }
+      const form = formRef.current!.instance;
+      const validate = form.validate();
+      validate.status === "pending" &&
+        validate.complete?.then((r) => {
+          if (r.status === "invalid") return;
+          setLoadingSave(true);
+          if (isCreate) {
+            createLeads(leads).then(handleSuccess).catch(handleError);
+          } else {
+            updateLeads(idData, leads).then(handleSuccess).catch(handleError);
+          }
+        });
     } catch (error) {
       setLoadingSave(false);
       notifyError("Gagal menyimpan data, coba lagi!");
@@ -77,8 +79,15 @@ const CreateEditContactLeads = () => {
 
   const handleCreateContact = async () => {
     try {
-      setLoadingCreateContact(true);
-      await createContactLeads(idData, leads).then(handleSuccess).catch(handleError);
+      const form = formRef.current!.instance;
+      const validate = form.validate();
+
+      validate.status === "pending" &&
+        validate.complete?.then((r) => {
+          if (r.status === "invalid") return;
+          setLoadingCreateContact(true);
+          createContactLeads(idData, leads).then(handleSuccess).catch(handleError);
+        });
     } catch (error) {
       setLoadingCreateContact(false);
       notifyError("Gagal menyimpan data, coba lagi!");
@@ -93,6 +102,14 @@ const CreateEditContactLeads = () => {
     return validatePhone(request);
   };
 
+  const asyncValidationIdNumber = (params: any) => {
+    const request = {
+      phoneNumber: params.value,
+      contactId: id
+    };
+    return validateIdNumber(request);
+  };
+
   const handleBack = () => {
     navigate(-1);
   };
@@ -100,91 +117,94 @@ const CreateEditContactLeads = () => {
   return (
     <div className={"content-block"}>
       <h2>{isCreate ? "Create" : "Edit"} Leads</h2>
-      <form className="form__tabs" action="register">
-        <Form
-          ref={formRef}
-          id="form"
-          formData={leads}
-          showColonAfterLabel={true}
-          showValidationSummary={true}
-          validationGroup="leadsData"
-        >
-          <GroupItem cssClass={"dx-card responsive-paddings next-card"}>
-            <GroupItem caption="Leads Data" colCount={2}>
-              <SimpleItem dataField="name" label={{ text: "Name" }} />
-              <SimpleItem
-                dataField="mobileNumber"
-                label={{ text: "Mobile Number" }}
-                editorOptions={{
-                  min: 0,
-                  maxLength: 15,
-                  onKeyDown: (e: any) => {
-                    const key = e.event.key;
-                    e.value = String.fromCharCode(e.event.keyCode);
-                    if (!/[0-9]/.test(e.value) && key !== "Backspace" && key !== "Delete")
-                      e.event.preventDefault();
-                  }
-                }}
-              >
-                <AsyncRule
-                  message="Mobile phone is already registered"
-                  validationCallback={asyncValidationPhoneNumber}
-                />
-              </SimpleItem>
-              <SimpleItem
-                dataField="idNumber"
-                label={{ text: "NIK/KTP Number" }}
-                editorOptions={{
-                  min: 0,
-                  maxLength: 20,
-                  onKeyDown: (e: any) => {
-                    const key = e.event.key;
-                    e.value = String.fromCharCode(e.event.keyCode);
-                    if (!/[0-9]/.test(e.value) && key !== "Backspace" && key !== "Delete")
-                      e.event.preventDefault();
-                  }
-                }}
+      <Form
+        ref={formRef}
+        id="form"
+        formData={leads}
+        showColonAfterLabel={true}
+        showValidationSummary={true}
+        validationGroup="leadsData"
+      >
+        <GroupItem cssClass={"dx-card responsive-paddings next-card"}>
+          <GroupItem caption="Leads Data" colCount={2}>
+            <SimpleItem dataField="name" label={{ text: "Name" }} />
+            <SimpleItem
+              dataField="mobileNumber"
+              label={{ text: "Mobile Number" }}
+              editorOptions={{
+                min: 0,
+                maxLength: 15,
+                onKeyDown: (e: any) => {
+                  const key = e.event.key;
+                  e.value = String.fromCharCode(e.event.keyCode);
+                  if (!/[0-9]/.test(e.value) && key !== "Backspace" && key !== "Delete")
+                    e.event.preventDefault();
+                }
+              }}
+            >
+              <AsyncRule
+                message="Mobile phone is already registered"
+                validationCallback={asyncValidationPhoneNumber}
               />
-              <SimpleItem dataField="marketAddress" label={{ text: "Market Address" }} />
-            </GroupItem>
-
-            <GroupItem colCountByScreen={{ xs: 4, sm: 8, md: 12, lg: 12 }}>
-              <ButtonItem horizontalAlignment="left">
-                <ButtonOptions width={"100%"} onClick={handleBack}>
-                  <span className="dx-button-text">Kembali</span>
-                </ButtonOptions>
-              </ButtonItem>
-
-              <ButtonItem horizontalAlignment="left">
-                <ButtonOptions
-                  type="default"
-                  width={"100%"}
-                  disabled={isLoadingSave}
-                  onClick={handleSave}
-                >
-                  <div className="button-options">
-                    <LoadIndicator width="20px" height="20px" visible={isLoadingSave} />
-                    <span className="dx-button-text">Simpan</span>
-                  </div>
-                </ButtonOptions>
-              </ButtonItem>
-
-              <ButtonItem visible={!isCreate} colSpan={2} horizontalAlignment="left">
-                <ButtonOptions
-                  type="success"
-                  disabled={isLoadingCreateContact}
-                  onClick={handleCreateContact}
-                >
-                  <div className="button-options">
-                    <LoadIndicator width="20px" height="20px" visible={isLoadingCreateContact} />
-                    <span className="dx-button-text">Buat Kontak</span>
-                  </div>
-                </ButtonOptions>
-              </ButtonItem>
-            </GroupItem>
+            </SimpleItem>
+            <SimpleItem
+              dataField="idNumber"
+              label={{ text: "NIK/KTP Number" }}
+              editorOptions={{
+                min: 0,
+                maxLength: 20,
+                onKeyDown: (e: any) => {
+                  const key = e.event.key;
+                  e.value = String.fromCharCode(e.event.keyCode);
+                  if (!/[0-9]/.test(e.value) && key !== "Backspace" && key !== "Delete")
+                    e.event.preventDefault();
+                }
+              }}
+            >
+              <AsyncRule
+                message="KTP/NIK Number is already registered"
+                validationCallback={asyncValidationIdNumber}
+              />
+            </SimpleItem>
+            <SimpleItem dataField="marketAddress" label={{ text: "Market Address" }} />
           </GroupItem>
-        </Form>
-      </form>
+
+          <GroupItem colCountByScreen={{ xs: 4, sm: 8, md: 12, lg: 12 }}>
+            <ButtonItem horizontalAlignment="left">
+              <ButtonOptions width={"100%"} onClick={handleBack}>
+                <span className="dx-button-text">Kembali</span>
+              </ButtonOptions>
+            </ButtonItem>
+
+            <ButtonItem horizontalAlignment="left">
+              <ButtonOptions
+                type="default"
+                width={"100%"}
+                disabled={isLoadingSave}
+                onClick={handleSave}
+              >
+                <div className="button-options">
+                  <LoadIndicator width="20px" height="20px" visible={isLoadingSave} />
+                  <span className="dx-button-text">Simpan</span>
+                </div>
+              </ButtonOptions>
+            </ButtonItem>
+
+            <ButtonItem visible={!isCreate} colSpan={2} horizontalAlignment="left">
+              <ButtonOptions
+                type="success"
+                disabled={isLoadingCreateContact}
+                onClick={handleCreateContact}
+              >
+                <div className="button-options">
+                  <LoadIndicator width="20px" height="20px" visible={isLoadingCreateContact} />
+                  <span className="dx-button-text">Buat Kontak</span>
+                </div>
+              </ButtonOptions>
+            </ButtonItem>
+          </GroupItem>
+        </GroupItem>
+      </Form>
     </div>
   );
 };
