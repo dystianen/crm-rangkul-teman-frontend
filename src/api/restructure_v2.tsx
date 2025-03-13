6ea96d6f4ced5331c413d869f9d7ac80ffaf2a85
Danang Sigit Prasetya <danangsigit@gmail.com>
2025-03-13 12:00:45 +0700

    submit restruture

---
+++ src/api/restructure_v2.tsx
@@ -0,0 +1,31 @@
+import {ajaxGet, ajaxPost} from "./http.api";
+import {dataRawCustomStore} from "../model/datagrid";
+import {customStore} from "../model/customStore";
+
+export const restructureV2ListStore = customStore({ loadUrl: "/api/trx/contract/restructure" });
+export const frequencyStore = dataRawCustomStore(`/api/trx/contract/restructure/v2/frequency?`);
+
+export const calc = async (payload: any): Promise<any> => {
+    const resp = await ajaxPost(`/api/trx/contract/restructure/calc`, payload);
+    return resp.data;
+};
+
+export const submit = async (payload: any): Promise<any> => {
+    const resp = await ajaxPost(`/api/trx/contract/restructure/submit`, payload);
+    return resp.data;
+};
+
+export const getDetail = async (id: any): Promise<any> => {
+    const resp = await ajaxGet(`/api/trx/contract/restructure/v2/detail/${id}`);
+    return resp.data;
+};
+
+export const approve = async (id: any, payload: any): Promise<any> => {
+    const resp = await ajaxPost(`/api/trx/contract/restructure/v2/approve/${id}`, payload);
+    return resp.data;
+};
+
+export const reject = async (id: any, payload: any): Promise<any> => {
+    const resp = await ajaxPost(`/api/trx/contract/restructure/v2/reject/${id}`, payload);
+    return resp.data;
+};
