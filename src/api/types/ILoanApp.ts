27bad9350fac384d02ada527f766ce9f7e7c36f5
Dystian En <distian.enyus@gmail.com>
2025-05-23 21:58:12 +0700

    feat: commodity field

--- src/api/types/ILoanApp.ts
+++ src/api/types/ILoanApp.ts
@@ -15,3 +15,17 @@ export type TResCheckAccessStep2 = {
   access: boolean;
   message: string;
 };
+
+export type TResCommodityDetail = {
+  type: {
+    createdOn: string;
+    modifiedOn: string;
+    createdBy: string;
+    modifiedBy: string;
+    id: string;
+    name: string;
+    isActive: boolean;
+    displayOrder: number;
+    hibernateLazyInitializer: string;
+  };
+};
